12c8e7cfd52a0bdc2369bdb261948b144d9838e1
newwebash <newtona2011@gmail.com>
2021-01-10 12:55:55 -0500

    Enable dynamic routing for event list component

--- src/router/index.js
+++ src/router/index.js
@@ -13,15 +13,16 @@ const routes = [
     component: EventList
   },
   {
-    path: "/event",
-    name: "event-show",
-    component: EventShow
-  },
-  {
     path: "/event-create",
     name: "event-create",
     component: EventCreate
-  }
+  },
+  {
+    path: "/event/:id",
+    name: "event-show",
+    component: EventShow,
+    props: true
+  }  
 ];
 
 const router = new VueRouter({
