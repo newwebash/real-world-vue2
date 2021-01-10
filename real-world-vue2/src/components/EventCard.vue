ae25f3de734b6a382365fae9bd000e93b6079209
newwebash <newtona2011@gmail.com>
2021-01-09 20:12:03 -0500

    Initialize project

---
+++ real-world-vue2/src/components/EventCard.vue
@@ -0,0 +1,23 @@
+<template>
+  
+  <div>
+      <h1></h1>
+      <ul>
+          <li></li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            quote: "Ciaody"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
